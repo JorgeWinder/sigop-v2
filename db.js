3cbd49c87cf7a74cf291454522282c3466579c80
Gaby Perez <gabyperez.dev@gmail.com>
2020-10-07 15:18:46 -0600

    probando

--- db.js
+++ db.js
@@ -1,22 +1,22 @@
 const db = require('mongoose')
 const chalk = require('chalk')
 
-
+//prueba
 // mongodb+srv://user-mbd:<password>@cluster0-qmwio.gcp.mongodb.net/test
 
 db.Promise = global.Promise
 
-async function connect(url){
-    
+async function connect(url) {
+
     db.connect(url, {
-    useCreateIndex: true,
-    useNewUrlParser: true, useUnifiedTopology: true
+        useCreateIndex: true,
+        useNewUrlParser: true, useUnifiedTopology: true
     })
-    console.log('[DB] Conectada con éxito a: ' +  chalk.bgWhite.black(url))
+    console.log('[DB] Conectada con éxito a: ' + chalk.bgWhite.black(url))
 
     //console.log(db.models)
     //console.log(db.version)
-    
+
 }
 
 module.exports = {
