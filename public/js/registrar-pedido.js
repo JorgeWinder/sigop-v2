1d033612f86de2d99e09b20f3a6a4f6036df4e63
Gaby Perez <gabyperez.dev@gmail.com>
2020-10-09 14:32:11 -0600

    se agrega modal

--- public/js/registrar-pedido.js
+++ public/js/registrar-pedido.js
@@ -2,6 +2,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.modal')
     var instances = M.Modal.init(elems)
-})
+        (async function Load() {
 
+            document.querySelector("#agregar-producto").click
+
+
+        })
+})
 
