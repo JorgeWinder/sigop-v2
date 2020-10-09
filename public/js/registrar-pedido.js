67c7767831606e563ef898032d4771011088504f
JORGE WINDER <jorge.winder@gmail.com>
2020-10-09 17:48:10 -0500

    Últimos cambios

--- public/js/registrar-pedido.js
+++ public/js/registrar-pedido.js
@@ -13,30 +13,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // ****************** Eventos ******************* //
 
-        const detalle = document.querySelector("#detalle tbody")
-        const agregar = document.querySelector("#btnAgregarProducto")
-
-        let contarFilas = document.querySelectorAll("#detalle tbody tr").length
-        let contenido = document.querySelector("#detalle tbody").innerHTML 
-
-        contador = contarFilas
-
-        agregar.addEventListener("click", function(){
-            
-            contador += 1
-            const inputProducto = `<div class="input-field" style="margin-right: 10px;"><input type="text" id="nompro${contador}" name="nompro${contador}" value=""><input type="hidden" id="idProducto" value=""></div>`
-
-            contenido = contenido + `<tr><td>${contador}</td><td>${inputProducto}</td></tr>`
-
-            detalle.innerHTML = contenido 
-
-            document.querySelectorAll("#detalle tbody tr").forEach(function(elemento){
-              console.log(elemento.querySelectorAll("td")[1].querySelector("input").value) 
-            })
-    
-
-        })
-
 
        
 
