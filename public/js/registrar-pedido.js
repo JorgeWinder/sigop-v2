bc86b48784125bb11ab62027b12f92b1e2f165c8
JORGE WINDER <jorge.winder@gmail.com>
2020-10-09 17:07:35 -0500

    Últimos cambios

--- public/js/registrar-pedido.js
+++ public/js/registrar-pedido.js
@@ -1,7 +1,49 @@
 
 document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.modal')
-    var instances = M.Modal.init(elems)
+    
+
+
+    (async function Load(){
+
+        // var elems = document.querySelectorAll('.modal')
+        // var instances = M.Modal.init(elems)
+
+        // ***************** Funciones ******************* //
+
+
+        // ****************** Eventos ******************* //
+
+        const detalle = document.querySelector("#detalle tbody")
+        const agregar = document.querySelector("#btnAgregarProducto")
+
+        let contarFilas = document.querySelectorAll("#detalle tbody tr").length
+        let contenido = document.querySelector("#detalle tbody").innerHTML 
+
+        contador = contarFilas
+
+        agregar.addEventListener("click", function(){
+            
+            contador += 1
+            const inputProducto = `<div class="input-field" style="margin-right: 10px;"><input type="text" id="nompro${contador}" name="nompro${contador}" value=""><input type="hidden" id="idProducto" value=""></div>`
+
+            contenido = contenido + `<tr><td>${contador}</td><td>${inputProducto}</td></tr>`
+
+            detalle.innerHTML = contenido 
+
+            document.querySelectorAll("#detalle tbody tr").forEach(function(elemento){
+              console.log(elemento.querySelectorAll("td")[1].querySelector("input").value) 
+            })
+    
+
+        })
+
+
+       
+
+
+    })()
+
+
 })
 
 
