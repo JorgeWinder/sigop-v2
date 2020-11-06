068cc91f9d4f28a5da628c8e548cc4f9b335fc83
Wendy Palomino <wvanessa.palomino.rivera@gmail.com>
2020-11-05 23:15:48 -0500

    fixed registro de pedido html and added autocomplete

--- public/js/registrar-pedido.js
+++ public/js/registrar-pedido.js
@@ -6,46 +6,78 @@ document.addEventListener('DOMContentLoaded', function () {
         const elems = document.querySelectorAll('.modal')
         const instanceModal = M.Modal.init(elems)
 
-        // funciones
+        //=====Funciones====//
 
-        async function ListarClientes(){
+        async function ListarClientes() {
 
-            const {body: clientes} = await getData("./cliente")
-
-            console.log(clientes)
-
-
-                // let objDatos = {}
+            const { body: clientes } = await getData("./cliente")
+            
             let objDatos = new Object()
 
             clientes.forEach(element => {
                 objDatos[`${element._id} - ${element.nombre}`] = null
             });
 
-
-            const elems = document.querySelectorAll('.autocomplete');
+            const elems = document.getElementById('search-client');
             const instances = M.Autocomplete.init(elems, {
-                data: objDatos
+                data: objDatos,
+                onAutocomplete: function (itemSelect) {
+                    const idCliente = itemSelect.split('-')[0].trim()
+
+                    getCliente(idCliente)
+                    document.getElementById('search-client').value = ''
+                    M.Modal.getInstance(document.querySelector('.modal')).close()
+                }
+
             });
-
-
-
-
         }
 
-        // eventos
-
-        // document.querySelector("#search-cliente").addEventListener('input', function (e) {
-        //     console.log(this.value)
-        // })
-
+        async function getCliente(idCliente){
+            const { body: {0 : cliente}} = await getData('../cliente?id=' + idCliente)
+            document.querySelector('#cliente').value = cliente.nombre
+        }
         
-        // --------------------- ///
+       
+
+        async function ListarProductos(){
+            const { body :productos } = await getData("./producto")
+            console.log(productos)
+        
+            let objProducto = new Object();
+
+            productos.forEach( producto => {
+                objProducto[`${producto._id} - ${producto.nombre_producto}`] = null
+            })
+
+            const elems = document.getElementById('producto');
+            const instances = M.Autocomplete.init(elems, {
+                data: objProducto,
+                onAutocomplete: function(itemSelect) {
+                    const idProducto = itemSelect.split('-')[0].trim()
+                    getProduct(idProducto)
+                },
+                limit:5,
+            }); 
+        }
+
+        async function getProduct(idProducto){
+            const { body: {0 : producto}} = await getData('../producto?id=' + idProducto)
+            document.querySelector('#precio').value = producto.precioMax
+        }
+
+       
+
+        //=====Calling Functions====//
 
         ListarClientes()
-
-        
+        ListarProductos()
 
     })()
+
+    
 })
 
+
+const calculateTotalPrice = (cantidad, precio, descuento) => {
+    return ((precio * descuento) / 100) * cantidad
+}
